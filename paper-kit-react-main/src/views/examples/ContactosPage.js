9235e7648d2834c5a077f5cca0fe6765ff49e53a
paopdf <pao_pdpf@hotmail.com>
2023-05-09 19:33:55 -0300

    Update ContactosPage.js

--- paper-kit-react-main/src/views/examples/ContactosPage.js
+++ paper-kit-react-main/src/views/examples/ContactosPage.js
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 // core components
@@ -12,25 +19,41 @@ import {
   Row,
   Col
 } from "reactstrap";
-const columns = [
-  { field: 'id', headerName: 'ID', width: 60 },
-  { field: 'Nombre', headerName: 'Nombre', width: 130 },
-  { field: 'Mail', headerName: 'Mail', width: 170},
-  {
-    field: 'Mensaje',
-    headerName: 'Mensaje',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 600
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#2a9d8f",
+    color: theme.palette.common.black
   },
-];
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    backgroundColor: "#e9c46a"
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(nombre, telefono, mail, mensaje) {
+  return { nombre, telefono, mail, mensaje};
+}
+
 const rows = [
-  { id: 1, Nombre: 'Matias Rivero', Mail: 'mati.rivero@gmail.com', Mensaje: 'd' },
-  { id: 2, Nombre: 'Leandro Morales', Mail: 'l.mora@gmail.com', Mensaje: 'Buenas Agustin, estoy muy interesado en tu perfil te dejo mis datos para que me contactes' },
-  { id: 3, Nombre: 'Hernan Duarte', Mail: 'hrnan_du@gmail.com', Mensaje: 'Buenas Agustin, estoy muy interesado en tu perfil te dejo mis datos para que me contactes' },
-  { id: 4, Nombre: 'Barbara Hoffman', Mail: 'bhoffman@gmail.com', Mensaje: 'Buenas Agustin, estoy muy interesado en tu perfil te dejo mis datos para que me contactes' },
-  { id: 5, Nombre: 'Rene Ledezma', Mail: 'rene_lede@gmail.com', Mensaje: 'Buenas Agustin, estoy muy interesado en tu perfil te dejo mis datos para que me contactes' }
- ];
+  createData('Barbara Hoffman','1157386926', 'bhoffman@hotmail.com', 'Buenas, estoy interesada en tu perfil, me gustaria contactarme con vos para hablar sobre el trabajo'),
+  createData('Matias Rivero','1157453826','mati_rivero@gmail.com','Hola Paola, me llamo Matias y trabajo en ASD. Me gustaría hablar contigo sobre una oportunidad de trabajo que tenemos disponible en nuestra empresa que creo que podría interesarte. Te dejo mi contacto'),
+  createData('Matias Wisnieski','1157263826', 'mw198@hotmail.com','Buenaas Paola, Me impresionó tu experiencia y habilidades y creo que podrías ser un buen ajuste para una de nuestras posiciones abiertas')
+  
+];
+
+
+
 export default function ContactosPage() {
   return (
     <>
@@ -39,29 +62,35 @@ export default function ContactosPage() {
     
       <div className="ml-auto mr-auto" lg = "6" >
       <Col >
-      <DataGrid 
-       sx={{
-        color: "#141414",
-        marginTop: "20px",
-        
-        marginBottom: "20px",
-        backgroundColor: "#e9c46a",
-        height: 400, width: '100%',
-        
-        }}
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-      />
+
+    <TableContainer component={Paper}>
+      <Table sx={{  minWidth: 700 , marginTop : "50px", marginBottom : "70px",marginLeft : "20x",marginRight : "20px"}} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Nombre</StyledTableCell>
+            <StyledTableCell align="left">Telefono</StyledTableCell>
+            <StyledTableCell align="left">Mail</StyledTableCell>
+            <StyledTableCell align="left">Mensaje</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.nombre}>
+              <StyledTableCell component="th" scope="row">
+                {row.nombre}
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.telefono}</StyledTableCell>
+              <StyledTableCell align="left">{row.mail}</StyledTableCell>
+              <StyledTableCell align="left">{row.mensaje}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
  </Col>
     </div>
     <DemoFooter />
     </>
  
-  );
+    );
 }
